894d3dca04c50c459164779c39b646017ded012e
nbur4556 <nbur4556@gmail.com>
2020-12-11 16:06:27 -0600

    feat: include title in nav bar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -8,6 +8,7 @@ import Nav from 'react-bootstrap/Nav';
 function NavigationBar() {
     return (
         <Navbar className="bg-secondary text-white">
+            <h2 className="mx-3">My Book List</h2>
             <Nav.Link as={Link} className="text-white" to="/">Saved Books</Nav.Link>
             <Nav.Link as={Link} className="text-white" to="/search">Search</Nav.Link>
         </Navbar>
