570bb972f59aafccf36d52a468e1382fb04e1599
nbur4556 <nbur4556@gmail.com>
2020-12-11 16:11:38 -0600

    feat: create book item component

--- client/src/components/BookResults.js
+++ client/src/components/BookResults.js
@@ -3,12 +3,18 @@ import React from 'react';
 // Bootstrap Components
 import Card from 'react-bootstrap/Card';
 
+// Components
+import BookItem from './BookItem.js';
+
 function BookResults(props) {
     return (
         <Card className={props.margin}>
             <Card.Header>{props.header}</Card.Header>
             <Card.Body>
-                Results Here
+                <BookItem />
+                <BookItem />
+                <BookItem />
+                <BookItem />
             </Card.Body>
         </Card>
     )
