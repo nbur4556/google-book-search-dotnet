90779b605df177fefdb3e1df5fb7ed07cd67dbf3
nbur4556 <nbur4556@gmail.com>
2020-12-11 13:02:25 -0600

    style: wrap pages in container, remove navbar from container

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+//Bootstrap Components
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 // Components
 import Search from './pages/Search.js';
 
 function App() {
   return (
-    <div>
-      <Search />
-    </div>
+    <article>
+      <Navbar className="bg-secondary text-white">Navigation</Navbar>
+      <Container>
+        <Search />
+      </Container>
+    </article>
   );
 }
 
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
 // Bootstrap Components
-import Navbar from 'react-bootstrap/Navbar';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 
 function Search() {
-    return (<div>
-        <Navbar className="bg-secondary text-white">Navigation</Navbar>
-
+    return (<section>
         <InputGroup>
             <InputGroup.Prepend>
                 <InputGroup.Text id="searchLabel">Search</InputGroup.Text>
             </InputGroup.Prepend>
             <FormControl ariaDescribedby="searchLabel" />
         </InputGroup>
-    </div>);
+    </section>);
 }
 
 export default Search;
