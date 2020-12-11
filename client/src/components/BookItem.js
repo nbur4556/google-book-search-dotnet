a418cadd75a3eb63fcb3f98db0bd8a452ac4b19e
nbur4556 <nbur4556@gmail.com>
2020-12-11 17:03:32 -0600

    feat: use props to aloow book item components to use variable book information

--- client/src/components/BookItem.js
+++ client/src/components/BookItem.js
@@ -4,25 +4,17 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image';
 
-function BookItem() {
+function BookItem(props) {
     return (
         <ListGroup.Item>
 
             <ListGroup variant="horizontal flush">
                 <ListGroup.Item className="border-0">
-                    <Image src="https://via.placeholder.com/150" />
+                    <Image src={props.bookDetails.imageSrc} />
                 </ListGroup.Item>
-                <ListGroup.Item className="border-0">Book Name</ListGroup.Item>
-                <ListGroup.Item className="border-0">
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                    ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum."
-                </ListGroup.Item>
-                <ListGroup.Item className="border-0">Book Options</ListGroup.Item>
+                <ListGroup.Item className="border-0">{props.bookDetails.title}</ListGroup.Item>
+                <ListGroup.Item className="border-0">{props.bookDetails.description}</ListGroup.Item>
+                <ListGroup.Item className="border-0">{props.options}</ListGroup.Item>
             </ListGroup>
 
         </ListGroup.Item>
