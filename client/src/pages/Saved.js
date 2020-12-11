b0b9fa1a120418ad7c78b8d310bc8eadaac82a77
nbur4556 <nbur4556@gmail.com>
2020-12-11 15:18:22 -0600

    feat: add book results component to saved books page

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // Components
 import PageHeader from '../components/PageHeader.js';
+import BookResults from '../components/BookResults.js';
 
 function Saved() {
     return (
         <section>
             <PageHeader pageName="Saved Books" />
+            <BookResults header="Saved Books" />
         </section>
     )
 }
