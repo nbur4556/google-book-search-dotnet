b4c401f4afe86105765af522d2774150039cefbb
nbur4556 <nbur4556@gmail.com>
2020-12-11 13:31:55 -0600

    doc: include search page notes

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -11,21 +11,25 @@ import BookResults from '../components/BookResults.js';
 function Search() {
     const margin = 'my-4';
 
+    // Searches API for Books
     const searchBook = () => {
         console.log("Search Book");
     }
 
-    return (<section>
-        <InputGroup className={margin}>
-            <InputGroup.Prepend>
-                <InputGroup.Text>Book Search</InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl />
-            <Button className="bg-secondary border-secondary text-light mx-2" onClick={searchBook}>Search</Button>
-        </InputGroup>
+    return (
+        <section>
+            <InputGroup className={margin}>
+                <InputGroup.Prepend>
+                    <InputGroup.Text>Book Search</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl />
+                <Button className="bg-secondary border-secondary text-light mx-2" onClick={searchBook}>Search</Button>
+            </InputGroup>
 
-        <BookResults header="Results" margin={margin} />
-    </section>);
+            {/* Contains API Results for Found Books */}
+            <BookResults header="Results" margin={margin} />
+        </section>
+    );
 }
 
 export default Search;
