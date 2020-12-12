4f3b168a5e19870e4537406b22a0e3ae5f0fc9f2
nbur4556 <nbur4556@gmail.com>
2020-12-12 15:39:42 -0600

    fix: set up server to use react client

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const googleBooksApi = require('./routes/google-books-api.js')
-
-// Initialize Environment
-const process = {
-    env: dotenv.config({ path: `${__dirname + '/.env'}` }).parsed
-}
+const googleBooksApi = require('./routes/google-books-api.js');
 
 // Initialize Application
 const app = express();
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Initialize Environment
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+else {
+    process = { env: dotenv.config({ path: `${__dirname + '/.env'}` }).parsed }
+}
 
 // API Routes
 googleBooksApi(app, process);
 
 // Create Server
+app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`);
+})
+
 app.listen(process.env.PORT, () => {
     console.log("listening on localhost:" + process.env.PORT);
 });
