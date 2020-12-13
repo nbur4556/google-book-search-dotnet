c5f70eee07613b9179bde021560d5d9cb1e53d79
nbur4556 <nbur4556@gmail.com>
2020-12-13 12:18:03 -0600

    fix: set up correct fetch call to reach backend api route

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -3,14 +3,9 @@ function Api() {
 }
 
 Api.prototype.searchBooksByName = function (name) {
-    console.log(this.searchUrl + name);
-
-    fetch('http://localhost:3000/search/lord+of+the+rings').then(response => {
-        console.log(response);
-        response.json();
-    }).then(data => {
+    fetch(this.searchUrl + name).then(response => response.json()).then(data => {
         console.log(data);
-    })
+    });
 }
 
 export default Api;
