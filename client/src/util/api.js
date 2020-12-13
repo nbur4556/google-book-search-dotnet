e292fb6ed25485e477eec5222e5c51e06109bc8e
nbur4556 <nbur4556@gmail.com>
2020-12-13 12:42:14 -0600

    feat: format search input to work in google api url

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -2,8 +2,15 @@ function Api() {
     this.searchUrl = '/search/';
 }
 
+Api.prototype.formatName = function (name) {
+    const formattedName = name.split(' ').join('+').toLowerCase();
+    return formattedName
+}
+
 Api.prototype.searchBooksByName = function (name) {
-    fetch(this.searchUrl + name).then(response => response.json()).then(data => {
+    const urlName = this.formatName(name);
+
+    fetch(this.searchUrl + urlName).then(response => response.json()).then(data => {
         console.log(data);
     });
 }
