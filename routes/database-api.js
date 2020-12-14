d2252ea85c7c50e6f2ea8a800093207f2f3eaf36
nbur4556 <nbur4556@gmail.com>
2020-12-13 18:48:42 -0600

    feat: create routes for adding, deleting, and viewing books in database

--- routes/database-api.js
+++ routes/database-api.js
@@ -1,3 +1,18 @@
-const databaseApi = (app, process) => { }
+const databaseApi = (app, process) => {
+    // Get All Books
+    app.get('/api/books', (req, res) => {
+        res.send("Get All Books");
+    });
+
+    // Add Book
+    app.post('/api/books/add', (req, res) => {
+        res.send("Add New Book");
+    });
+
+    // Delete Book
+    app.delete('/api/books/delete/:bookId', (req, res) => {
+        console.log("Delete Book");
+    });
+}
 
 module.exports = databaseApi;
