1589a7acc50723326878cce7f22981925e067bf0
nbur4556 <nbur4556@gmail.com>
2020-12-13 19:48:10 -0600

    doc: clarify database-api notes

--- routes/database-api.js
+++ routes/database-api.js
@@ -1,21 +1,21 @@
 const dbController = require('../controllers/dbController.js');
 
 const databaseApi = (app) => {
-    // Get All Books
+    // Get All Books Route
     app.get('/api/books', (req, res) => {
         dbController.getAllBooks(result => {
             res.json(result);
         });
     });
 
-    // Add Book
+    // Add Book Route
     app.post('/api/books/add', (req, res) => {
         dbController.addBook(req.body, (result => {
             res.json(result);
         }));
     });
 
-    // Delete Book
+    // Delete Book Route
     app.delete('/api/books/delete/:bookId', (req, res) => {
         dbController.deleteBook(req.params.bookId, (result => {
             res.json(result);
