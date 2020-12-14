d18177eec19cff2f955e44cc01af2e945194da98
nbur4556 <nbur4556@gmail.com>
2020-12-14 09:36:37 -0600

    feat: load saved books when loading the saved books page

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Components
 import PageHeader from '../components/PageHeader.js';
 import BookResults from '../components/BookResults.js';
 
+// Assets 
+import Api from '../util/api.js';
+
 function Saved() {
+    const api = new Api();
+
+    const [savedState, setSavedState] = useState({
+        savedResults: []
+    });
+
+    useEffect(() => {
+        api.getSavedBooks(results => {
+            setSavedState({ ...savedState, savedResults: results });
+        });
+    }, []);
+
     return (
         <section>
             <PageHeader pageName="Saved Books" />
