a4cd70e3b337073739095df775a9dbb5d86345ba
nbur4556 <nbur4556@gmail.com>
2022-01-11 13:19:35 -0600

    init: new dotnet server

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace google_book_search.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
